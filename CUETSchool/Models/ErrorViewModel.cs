869d59470539827203d53723690ab003290ad120
RashfiTabassum <rashfitabassum@gmail.com>
2025-01-28 19:32:40 +0600

    Add project files.

---
+++ CUETSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CUETSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
