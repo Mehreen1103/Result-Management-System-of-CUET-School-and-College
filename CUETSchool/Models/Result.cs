13a325fe29c93a2de1eed690309f9a556344dc62
RashfiTabassum <160368391+RashfiTabassum@users.noreply.github.com>
2025-02-19 01:09:10 +0600

    Add files via upload

---
+++ CUETSchool/Models/Result.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CUETSchool.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+        public string SubjectName { get; set; }
+        public string Class { get; set; }
+
+        // Store all marks dynamically as JSON (CQ, MCQ, Practical, etc.)
+        public string MarksJson { get; set; } // Stores {"CQ": 10, "MCQ": 15, "Practical": 20}
+
+        public int FullMarks { get; set; }
+        public int? TotalMarks { get; set; }
+        public string? Grade { get; set; }
+    }
+}
