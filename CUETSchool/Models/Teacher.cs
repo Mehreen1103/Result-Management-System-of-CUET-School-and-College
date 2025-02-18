1c657f82a7aabf3a2ff4c444ef4549cbc0ebaa06
RashfiTabassum <160368391+RashfiTabassum@users.noreply.github.com>
2025-02-19 01:11:07 +0600

    Add files via upload

---
+++ CUETSchool/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace CUETSchool.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+        public string Class { get; set; }
+    }
+}
